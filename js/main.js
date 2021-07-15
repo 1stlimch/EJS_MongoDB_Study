922c690dee4bcc1d4efa788145e5a079de050958
Limch <limch3125@gmail.com>
2021-07-15 11:22:22 +0900

    Add .gitignore Remove none use code in main.js

--- js/main.js
+++ js/main.js
@@ -3,29 +3,6 @@ const menu = document.querySelector('.navbar__menu');
 const icons = document.querySelector('.navbar__icons');
 
 toggleBtn.addEventListener('click', () => {
-    menu.classList.toggle('active');
-    icons.classList.toggle('active');
+  menu.classList.toggle('active');
+  icons.classList.toggle('active');
 });
-
-function openTextFile() {
-    var input = document.createElement("input");
-
-    input.type = "file";
-    input.accept = "text/plain";
-
-    input.onchange = function(event) {
-        processFile(event.target.files[0]);
-    };
-
-    input.click();
-}
-
-function processFile(file) {
-    var reader = new FileReader();
-
-    reader.onload = function () {
-        output.innerText = reader.result;
-    };
-
-    reader.readAsText(file, "utf-8");
-}
