1158526ce92eb9e253064b418aabbab7ebf5140d
Limch <limch3125@gmail.com>
2021-07-16 10:41:51 +0900

    Commit before clean project & return master

--- server.js
+++ server.js
@@ -2,8 +2,8 @@ var express = require('express');
 var app = express();
 var router = require('./routes/main')(app);
 
-app.set('views', __dirname + '/views');
-app.set('view engine', 'ejs');
+app.set('views', __dirname + '/views'); //ejs 파일을 모아둔 폴더 위치를 알림
+app.set('view engine', 'ejs'); //express에게 뷰 엔진은 ejs로 사용하것이라 알림
 app.engine('html', require('ejs').renderFile);
 
 var server = app.listen(3000, function () {
